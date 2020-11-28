dff7fed00efe549fa4371f00106595cee4b0ec9a
Stacy Betts <stacyo23@yahoo.com>
2020-11-27 19:40:51 -0600

    questions/template in place, getting writefile error

--- index.js
+++ index.js
@@ -1,14 +1,71 @@
+//constants requiring the script file export, the inquirer dependency, and file system library
 const generateMarkdown = require('./utils/generateMarkdown'); 
 const inquirer = require('inquirer');
 const fs = require('fs');
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, JSON.stringify(data)), err =>
+        err ? console.log(err) : console.log('Success!'); 
+        
 }
 
 // function to initialize program
 function init() {
+    //initializes command-line questions
+    inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your GitHub user name?',
+      },
+      {
+        type: 'input',
+        message: 'What is your contact email?',
+        name: 'email',
+      },
+      {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'description',
+      },
+      {
+        type: 'input',
+        message: 'Please provide a brief description of the application.',
+        name: 'description',
+      },
+      {
+        type: 'input',
+        message: 'Please specify any necessary installations.',
+        name: 'install',
+      },
+      {
+        type: 'input',
+        message: 'Please describe how the application will be used.',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'Did anyone assist in building the application?',
+        name: 'credits',
+      },
+      {
+        type: "list",
+        name: "license",
+        message: "Select a license for your README file.",
+        choices: ["GNU GPL v2", "GNU GPL v3", "GNU LGPL v3", "MIT", "No License"]
+    },
+    ])
+    .then(response => {
 
+  //runs responses through the generateMarkdown text      
+  const data = generateMarkdown(response);  
+ 
+  //calls the write function with title of md and responses run through the generate Markdown text
+  writeToFile(newReadMe.md, data); 
+    });
+  
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,25 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+    // template literals to find and replace
+  return `
+  # ${data.title}
+
+  ## Description
+  ${data.description}
+
+  ##Installation
+  The following will be necessary to download in order for the file to run correctly: ${data.install}.
+
+  ##Usage
+  ${data.usage}
+
+  ##Credits
+  ${data.credit}
+
+  ##License
+  ${data.license}
 
 `;
 }
-
+//exports this script to index
 module.exports = generateMarkdown;
