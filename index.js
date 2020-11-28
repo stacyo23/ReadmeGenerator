37d8f31065c8b45fca3a20d53b645c7f94558331
Stacy Betts <stacyo23@yahoo.com>
2020-11-27 18:37:08 -0600

    first commit, initialized, installed inquirer

---
+++ index.js
@@ -0,0 +1,15 @@
+const generateMarkdown = require('./utils/generateMarkdown'); 
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
